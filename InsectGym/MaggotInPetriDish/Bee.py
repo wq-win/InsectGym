abde573bc49c74873c4e5434aabbee38ddc1712d
wangq635 <643797037@qq.com>
2024-04-24 21:36:44 +0800

    add some action

--- InsectGym/MaggotInPetriDish/Bee.py
+++ InsectGym/MaggotInPetriDish/Bee.py
@@ -23,13 +23,14 @@ class BeeFoodEnv(discrete.DiscreteEnv):
         # self.food_loactions = self.num_locations
         # TODO: observation 是bee的state * food的state 还是只有bee的location？
         # num_states = self.bee_locations * self.food_loactions
-        num_states = self.num_locations * self.num_locations
+        num_states = self.num_locations * self.num_locations  # bee的state * food的state
         
         initial_state_distrib = np.zeros(num_states)
         initial_state_distrib[0] = 1  # TODO:可以让0，1，2三个状态平均概率随机 [0:3]=1
         initial_state_distrib /= initial_state_distrib.sum()
 
-        num_actions = 3  # 0:拽 1:休息 2:右移
+        action_dict = {0: 'pull', 1: 'not move', 2: 'right', 3: 'left', 4: 'stand up'}
+        num_actions = len(action_dict)  # 0:拽 1:不动 2:右移 3:左移 4:站立
         
         P = {
             state: {action: [] for action in range(num_actions)}
@@ -39,33 +40,31 @@ class BeeFoodEnv(discrete.DiscreteEnv):
         
         for state in range(num_states):
             for action in range(num_actions):
-                done = False
+                prob, reward, done = 1, self.reward_options[0], False
                 
-                if (state == 7 and action == 2) or (state == 5 and action == 0) or state == 8:
-                    reward = self.reward_options[1]
-                else:
-                    reward = self.reward_options[0]
-                
-                if reward == self.reward_options[1]:
-                    done = True
-                
-                prob = 1
-                if action == 0:
-                    if state in (0, 3, 4):  # 拽食物更困难
-                        prob = .6
+                if action == 0:  # pull
+                    # if state in (0, 3, 4):  # 拽食物更困难, 
+                    #     prob = .6
                     next_state = state + self.num_locations
-                    
-                    if state in (6, 7, 8):  # 若food在最右边，继续拽，状态不变
+                    if state in range(num_states)[-num_locations:]:  # 若food在最右边，继续拽，状态不变
                         next_state = state  
-                elif action == 1:
-                    next_state = state
-                    # prob = 1
-                else:
+                elif action == 1:  # not move
+                    next_state = state 
+                elif action == 2:  # right
                     next_state = state + 1
-                    # prob = 1
-                    prob = .88  # 一定概率移动失败
-                    if state in (2, 5, 8):  # 若bee在最右边，继续右移，状态不变
+                    prob = .88  # 1-prob概率移动失败
+                    if state in range(num_states)[2::num_locations]:  # 若bee在最右边，继续右移，状态不变
                         next_state = state
+                elif action == 3:  # left
+                    next_state = state - 1 
+                    prob = .88  # 1-prob概率移动失败
+                    if state in range(num_states)[::num_locations]:  # 若bee在最左边，继续左移，状态不变
+                        next_state = state
+                elif action == 4:  # stand up
+                    next_state = state
+                    if state == num_states-1:
+                        reward = self.reward_options[1]
+                        done = True 
                                 
                 P[state][action].append((prob, next_state, reward, done))
                 if prob != 1:
@@ -105,17 +104,24 @@ class BeeFoodEnv(discrete.DiscreteEnv):
 if __name__ == "__main__":
     env = BeeFoodEnv()
     obs = env.reset()
-    while True:
-        action = env.action_space.sample()
     
-        obs, reward, done, info = env.step(action)
+    done_step = 0
+    average_step = 0
+    eps = 1000
+    for _ in range(eps):
+        done_step = 0
+        while True:
+            action = env.action_space.sample()
+        
+            obs, reward, done, info = env.step(action)
 
-        print('action:%d, state:%d, reward:%d, done:%s, info:%s' % (action, obs, reward, done, info))
-    
-        env.render()
-    
-        if done:
-            print('done')
-            break
-    
+            # print('action:%d, state:%d, reward:%d, done:%s, info:%s' % (action, obs, reward, done, info))
+        
+            # env.render()
+            done_step += 1
+            if done:
+                # print('done')
+                average_step += done_step
+                break
+    print(average_step/eps)
     env.close()
